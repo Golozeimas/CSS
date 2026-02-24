14ab055cc9b3f0561804c3ee209c6ab903e295ac
Matheus+ <joaosobramatheus@gmail.com>
2026-02-24 11:24:28 -0300

    <update> Feito a seção sobre do projeto e 'projetos'

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from shutil import make_archive
 
-pasta_para_zipar = input("Coloque o nome da sua pasta ou arquivo: ")
+pasta_para_zipar = input("Coloque o caminho da pasta ou do arquivo (caminho relativo): ")
 
 make_archive("Html_projeto2","zip",pasta_para_zipar) # mais fácil zipar assim com o make_archive do shuti
